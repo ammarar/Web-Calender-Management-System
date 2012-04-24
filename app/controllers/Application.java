cbda406e29e4eb24f0a824548cbf273c8a7e49ae
Ammar Alrashed <ammar.alrashed@gmail.com>
2012-04-24 09:11:35 -0400

    Added some actions and birthday event

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,12 +5,26 @@ import play.mvc.*;
 
 import java.util.*;
 
+import org.joda.time.LocalDate;
+
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+    
+    public static void createBirthdayEvent(String name, LocalDate date, User birthdayPerson, boolean surprise)
+    {
+    	BirthdayEvent be = new BirthdayEvent(name, date, birthdayPerson, surprise);
+    	be.save();
+    	//Put the view of the confirmation page
+    }
+    
+    public static void createBirthdayEventForm()
+    {
+    	render();
     }
 
 }
